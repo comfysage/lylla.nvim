837401d563c8b4cd565def71aa6f04705f5e6aa7
robin <comfysage@isabelroses.com>
2025-08-31 14:44:45 +0200

    meta: update `.nvim.lua`

--- .nvim.lua
+++ .nvim.lua
@@ -8,14 +8,5 @@ vim.opt.rtp:prepend "."
 --
 
 pcall(function()
-	require("lylla").setup()
+	R("lylla").init()
 end)
-
---
-
-vim.api.nvim_create_autocmd("BufWritePost", {
-	pattern = "*/lylla/*",
-	callback = function()
-		vim.schedule(function() R "lylla".init() end)
-	end,
-})
