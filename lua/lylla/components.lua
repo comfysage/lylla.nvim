9e7b3d434828c1f8d2ddb5656f5361b67a2e7f5f
robin <comfysagedev@gmail.com>
2026-02-24 20:25:24 +0100

    refactor(utils): move lsp clients fn to separate components module

---
+++ lua/lylla/components.lua
@@ -0,0 +1,28 @@
+local components = {}
+
+---@param props { buffer?: integer, limit?: integer, sep?: string, filter?: fun(client: vim.lsp.Client): boolean }
+---@return string?
+function components.lsp_clients(props)
+  props = props or {}
+  local buffer = props.buffer or 0
+
+  local clients = vim
+    .iter(vim.lsp.get_clients({ bufnr = buffer }))
+    :map(function(
+      client --[[@as vim.lsp.Client]]
+    )
+      if props.filter and not props.filter(client) then
+        return
+      end
+      return client.config.name
+    end)
+    :totable()
+
+  if #clients == 0 then
+    return
+  end
+
+  return table.concat(clients, props.sep or " + ", 1, vim.F.if_nil(props.limit))
+end
+
+return components
